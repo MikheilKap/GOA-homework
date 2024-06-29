3598728d5ad7bf04e3a1ec144fc6209c295ba147
MikheilKap <Mikheilkap@gmail.com>
2024-06-29 18:01:41 +0400

    first commit

---
+++ day 01/lesson 2.py
@@ -0,0 +1,16 @@
+name = "mikheil"
+surname = "kapanadze"
+print(name + " " + surname)
+
+
+
+
+
+
+name = "mikheil"
+surname = "kapanadze"
+age = 13
+height = 168.5
+knows_programming = True
+is_sigma = True
+print( name + " " + surname + " " + str(age))
