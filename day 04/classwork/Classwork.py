2878d2a7d01ee87089dae643f30dd5b15075968b
MikheilKap <Mikheilkap@gmail.com>
2024-07-11 20:38:28 +0400

    second commit

---
+++ day 04/classwork/Classwork.py
@@ -0,0 +1,11 @@
+# here instead of using spacebar for naming variables, we must use the _ symbol
+user name = "Mikheil"
+#here we must use the = symbol  for naming variables
+age - 13
+year = 2010
+# here we must use () both symbols. however here, we are missing a ) symbol in the ending of the "current year" = year + age"
+print("current year" = year + age
+#here we don't have to use " " symbols for numbers in this case
+number="34"
+#here we don't have to use "" symbols for when we have a variable 
+print("number")
