05c209e17d8229745ff23d8245f338929cb7098a
MikheilKap <Mikheilkap@gmail.com>
2024-07-14 20:20:58 +0400

    second commit

---
+++ day 06/Homework/Homework 2.py
@@ -0,0 +1,8 @@
+#აქ შევქმნათ 3 ტიპის ცვლადი, რომლებიცაა:string(str),integer(int) და float(float).
+age=int(input())
+word=str(input())
+decimal=float(input())
+#შემდეგ კი გამოვიტანთ ტერმინალზე
+print(age)
+print(word)
+print(decimal)
---
+++ day 06/Homework/Homework 3.py
@@ -0,0 +1,5 @@
+#აქ შემოვიტანთ ორ string-ს : First name და Last name.
+First_name=str(input())
+Last_name=str(input())
+#შემდეგ კი გამოვიტანთ ტეწრმინალზე.
+print("Hello " + First_name + " " + Last_name)
